2c2bcdc5b174fdbb5daab037cfa3d21721796d0b
Alexey Svekolnikov <alexey.svekolnikov@gmail.com>
2021-06-09 01:17:45 +0300

    add test project

---
+++ MetricsManager.Tests/ForecastUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MetricsManager.Tests
+{
+    public class ForecastUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
